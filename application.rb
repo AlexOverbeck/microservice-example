74375a85551404228ade40ab696428503054a358
Alex Overbeck <alexoverbeck@gmail.com>
2019-02-27 14:45:25 -0500

    Adds weather service

--- application.rb
+++ application.rb
@@ -1,5 +1,18 @@
+require 'json'
 require 'sinatra'
+require 'geocoder'
+require 'httparty'
 
-get '/' do
-  'Hello World'
+get '/forecast/:location' do
+  content_type :json
+
+  coordinates = Geocoder.search(params[:location]).first.coordinates
+  
+  url = 'https://api.darksky.net/forecast/32751df79f6c9f93897968c0d588dd69/'
+
+  response = HTTParty.get(url + coordinates.first.to_s + ',' + coordinates.last.to_s)
+
+  {
+    weather: JSON.parse(response.body)
+  }.to_json
 end
